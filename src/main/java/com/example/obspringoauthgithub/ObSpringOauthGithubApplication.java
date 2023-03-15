a75327fac8ae72b505a5386370498e81931e8fe1
fernalp <almanzafernando@hotmail.com>
2023-03-14 19:39:15 -0500

    first commit

---
+++ src/main/java/com/example/obspringoauthgithub/ObSpringOauthGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.obspringoauthgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringOauthGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObSpringOauthGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obspringoauthgithub/ObSpringOauthGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obspringoauthgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObSpringOauthGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
